dc5e70728b71ca69f611f49891cf48977de63d55
FRANKD-UX <frankdafrica@gmail.com>
2025-06-25 01:26:35 -0700

    push

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+// routes/index.js
+import express from 'express';
+import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
+import FilesController from '../controllers/FilesController.js';
+
+const router = express.Router();
+
+// App routes
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+// User routes
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+// Auth routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+// File routes
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+import express from 'express';
+import routes from './routes/index.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
